38700215c87935ecc8689960cd971331826a0cf6
RomanHlodann <roman.hlodann@gmail.com>
2024-07-15 12:30:39 +0300

    add users app to INSTALLED_APPS and fix AUTH_USER_MODEL

--- social_media/settings.py
+++ social_media/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "ninja_extra",
     "ninja_jwt",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -124,4 +125,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "user.User"
+AUTH_USER_MODEL = "users.User"
