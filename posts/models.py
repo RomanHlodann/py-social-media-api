5ab30be61d4816520b98447bf347c6903b8248c0
RomanHlodann <roman.hlodann@gmail.com>
2024-07-15 15:34:58 +0300

    add post model & migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"Created at {self.created_at} by {self.user.username}"
