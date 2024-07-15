01d952162d20fc5c06d56ebbe0e74a1915d910ee
RomanHlodann <roman.hlodann@gmail.com>
2024-07-16 00:51:04 +0300

    add Comment model & migrations

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from posts.models import Post
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    comment = models.TextField()
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"Comment by {self.user.username}"
