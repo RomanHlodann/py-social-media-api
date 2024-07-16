fae6da55b166889e8edda73963992ecfc8fc9dae
RomanHlodann <roman.hlodann@gmail.com>
2024-07-16 18:12:59 +0300

    add auto_reply_enabled and auto_reply_delay attr to Post model & migrations

--- posts/models.py
+++ posts/models.py
@@ -12,6 +12,8 @@ class Post(models.Model):
         related_name="posts"
     )
     created_at = models.DateTimeField(auto_now_add=True)
+    auto_reply_enabled = models.BooleanField(default=False)
+    auto_reply_delay = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return f"Post by {self.user.username}"
